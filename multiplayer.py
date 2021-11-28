ff011237dec024807dd89c695de7ceb452c4755a
Aleš Tanglmajer <ales.tanglmajer@gmail.com>
2021-11-28 19:42:23 +0100

    players alternate in placing symbols

--- multiplayer.py
+++ multiplayer.py
@@ -34,13 +34,21 @@ def multiplayer(mode):
     clock = pygame.time.Clock()
 
     done = False
+    player_o = True
     while not done:
         clock.tick(30)
 
         tile = get_tile()
-
         draw(window, mode.board, tile)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_game()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                y, x = tile
+                if player_o:
+                    mode.board.array[x][y] = 1
+                else:
+                    mode.board.array[x][y] = 2
+                player_o = not player_o
