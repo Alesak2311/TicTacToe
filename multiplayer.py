6faefb5eefa5e17bdcdd4d99205ec4ea55e02d13
Aleš Tanglmajer <ales.tanglmajer@gmail.com>
2021-11-28 19:34:56 +0100

    restructure multiplayer

--- multiplayer.py
+++ multiplayer.py
@@ -7,20 +7,23 @@ BG_COLOR = (255, 255, 255)
 HIGHLIGHT = pygame.image.load("assets/tile-highlight.png")
 
 
-def highlight_tile(window):
+def get_tile():
     mouse_pos = pygame.mouse.get_pos()
     tile = tuple(map(lambda coord: ceil(coord / 40) - 1, mouse_pos))
-    highlight_pos = tuple(map(lambda coord: coord * 40, tile))
+    return tile
 
+
+def highlight_tile(window, tile):
+    highlight_pos = tuple(map(lambda coord: coord * 40, tile))
     window.blit(HIGHLIGHT, highlight_pos)
 
 
-def draw(window, board):
+def draw(window, board, tile):
     window.fill(BG_COLOR)
 
     board.draw_board(window)
 
-    highlight_tile(window)
+    highlight_tile(window, tile)
 
     pygame.display.update()
 
@@ -34,7 +37,9 @@ def multiplayer(mode):
     while not done:
         clock.tick(30)
 
-        draw(window, mode.board)
+        tile = get_tile()
+
+        draw(window, mode.board, tile)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
